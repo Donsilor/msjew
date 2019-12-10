85f2eedcf3e76c5096d57c5d97d4305d457100c9
gaopeng <763429951@qq.com>
2019-12-10 10:12:56 +0800

    add 配置文件

--- common/config/main.php
+++ common/config/main.php
@@ -15,6 +15,21 @@ return [
         'common\components\Init', // 加载默认的配置
     ],
     'components' => [
+        'db' => [
+                'class' => 'yii\db\Connection',
+                'dsn' => 'mysql:host=127.0.0.1;dbname=bdd',
+                'username' => 'super',
+                'password' => 'Bdd123o123',
+                'charset' => 'utf8',
+                'tablePrefix' => '',
+                'attributes' => [
+                        // PDO::ATTR_STRINGIFY_FETCHES => false, // 提取的时候将数值转换为字符串
+                        // PDO::ATTR_EMULATE_PREPARES => false, // 启用或禁用预处理语句的模拟
+                ],
+                // 'enableSchemaCache' => true, // 是否开启缓存, 请了解其中机制在开启，不了解谨慎
+                // 'schemaCacheDuration' => 3600, // 缓存时间
+                // 'schemaCache' => 'cache', // 缓存名称
+        ],
         /** ------ 缓存 ------ **/
         'cache' => [
             'class' => 'yii\caching\FileCache',

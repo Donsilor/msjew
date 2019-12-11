7e9afb72429163ea057170819b3401eba0519874
lrz0829 <502034356@qq.com>
2019-12-11 11:36:59 +0800

    属性

--- common/enums/AttrIdEnum.php
+++ common/enums/AttrIdEnum.php
@@ -9,20 +9,39 @@ namespace common\enums;
  */
 class AttrIdEnum 
 {
-    const DIA_SHAPE = 6;//钻石形状 ID
-    const DIA_CUT = 4;//钻石切工ID
-    const DIA_COLOR = 7;//钻石颜色
-    const DIA_CERT_TYPE = 48;//钻石证书类型ID
-    const DIA_CLARITY = 2;//钻石净度ID
-    const DIA_SYMMETRY = 29;//钻石对称ID
-    const DIA_POLISH = 28;//钻石抛光
-    const DIA_FLUORESCENCE = 8;//钻石荧光
-    const DIA_CUTTING_DEPTH = 32;//切割深度
-    const DIA_STONE_FLOOR = 37;//石底层
-    const DIA_STONE_SURFACE = 33;//石面
-    const DIA_ASPECT_RATIO = 36;//长宽比
-    const DIA_SALE_SERVICES = 25;//长宽比
-
+    const CLARITY = 2;//净度ID
+    const CUT = 4;//切工ID
+    const WEIGHT = 5;//主石重量ID
+    const SHAPE = 6;//钻石形状 ID
+    const COLOR = 7;//颜色
+    const FLUORESCENCE = 8;//荧光
+    const FINENESS = 10;//成色
+    const GRAM_WEIGHT = 11;//克重
+    const PRODUC_CUSTOMIZATION = 12;//商品定制
+    const SALE_SERVICES = 25;//售后服务
+    const FOR_PEOPLE = 26;//适用人群
+    const USE = 27;//用途
+    const POLISH = 28;//光泽（抛光）
+    const SYMMETRY = 29;//对称
+    const APPRAISAL_AGENCY = 30;//鉴定机构
+    const CERTIFICATE_NO = 31;//证书编号
+    const CUTTING_DEPTH = 32;//切割深度（%）
+    const STONE_SURFACE = 33;//石面（%）
+    const LENGTH = 34;//长度（mm）
+    const WIDTH = 35;//宽度（mm）
+    const ASPECT_RATIO = 36;//长宽比（%）
+    const STONE_FLOOR = 37;//石底层
+    const SIZE = 38;//尺寸
+    const RING_STYLE = 39;//对戒款式
+    const ENGAGEMENT_STYLE = 40;//订婚戒指款式
+    const HEIGHT = 41;//高度（mm）
+    const BUCKLE = 42;//扣环
+    const CHAIN_TYPE = 43;//链类型
+    const SIDE_STONE_WEIGHT = 44;//副石重量(ct)
+    const SIDE_STONE_QUANTITY = 45;//副石数量(个)
+    const SIDE_STONE_COLOR = 46;//副石颜色
+    const SIDE_STONE_CLARITY = 47;//副石净度
+    const CERT_TYPE = 48;//钻石证书类型ID
 
     
 }

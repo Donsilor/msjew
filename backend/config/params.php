f2032781761a2d623fa14af77dd042afc72cbfb4
gaopeng <763429951@qq.com>
2019-12-11 10:02:46 +0800

    add

--- backend/config/params.php
+++ backend/config/params.php
@@ -2,8 +2,8 @@
 
 return [
     'adminEmail' => 'admin@example.com',
-    'adminAcronym' => 'RF',
-    'adminTitle' => 'RageFrame',
+    'adminAcronym' => 'BDD',
+    'adminTitle' => 'BDD后台管理',
     'adminDefaultHomePage' => ['main/system'], // 默认主页
 
     /** ------ 总管理员配置 ------ **/

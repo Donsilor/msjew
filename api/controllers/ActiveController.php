273a38977527374bf6d8d552f4060c9cb4fbf7bc
gaopeng <763429951@qq.com>
2019-12-17 16:59:51 +0800

    add 导航菜单API

--- api/controllers/ActiveController.php
+++ api/controllers/ActiveController.php
@@ -49,6 +49,13 @@ class ActiveController extends \yii\rest\ActiveController
      */
     public function behaviors()
     {
+        //更改默认语言
+        $lang = \Yii::$app->request->get("lang");
+        if($lang) {
+            \Yii::$app->language = $lang;
+            \Yii::$app->params['language'] = $lang;
+        }
+       
         $behaviors = parent::behaviors();
         // 跨域支持
         $behaviors['corsFilter'] = [
--- api/modules/v1/controllers/common/MenuController.php
+++ api/modules/v1/controllers/common/MenuController.php
@@ -5,6 +5,7 @@ namespace api\modules\v1\controllers\common;
 use Yii;
 use api\controllers\OnAuthController;
 use common\models\common\Menu;
+use common\helpers\ArrayHelper;
 //use api\controllers\ActiveController;
 
 /**
@@ -25,8 +26,11 @@ class MenuController extends OnAuthController
      * @return array|yii\data\ActiveDataProvider
      */
     public function actionIndex()
-    {        
-        return Yii::$app->services->menu->getSimpleList();
+    {   
+        $cat_id = \Yii::$app->request->post('cat_id',6);
+        $models = Yii::$app->services->menu->getFrontList($cat_id);
+        $models = ArrayHelper::itemsMerge($models,0,'id','pid','items');
+        return $models;
     }
     
 }

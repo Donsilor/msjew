c49075f882154243d7de66d6a14df214963193b4
gaopeng <763429951@qq.com>
2019-12-17 15:31:26 +0800

    fix

--- services/common/MenuService.php
+++ services/common/MenuService.php
@@ -162,8 +162,8 @@ class MenuService extends Service
      */
     public function findAll($app_id = null,$language = null)
     {
-        if(empty($app_id)){
-            $app_id = Yii::$app->id;
+        if(!empty($app_id)){
+            \Yii::$app->id = $app_id;
         }
         if(empty($language)){
             $language = Yii::$app->params['language'];
@@ -177,7 +177,7 @@ class MenuService extends Service
             ->orderBy('cate_id asc, sort asc')
             ->andWhere(['app_id' => $app_id])
             ->with(['cate' => function (\yii\db\ActiveQuery $query) {
-                return $query->andWhere(['app_id' => $app_id]);
+                return $query->andWhere(['app_id' => \Yii::$app->id]);
             }])
             ->orderBy('sort asc, a.id asc')
             ->select(['a.*','ifnull(b.title,a.title) as title'])

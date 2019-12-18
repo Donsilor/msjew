63e294d6c702e4ca3fc1c45499eb6a41da8619d4
gaopeng <763429951@qq.com>
2019-12-18 10:36:03 +0800

    add

--- api/config/main.php
+++ api/config/main.php
@@ -25,7 +25,7 @@ return [
                     'Origin' => ['*'],
                     'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
                     'Access-Control-Request-Headers' => ['*'],
-                    'Access-Control-Allow-Credentials' => true,
+                    'Access-Control-Allow-Credentials' => false,
                     'Access-Control-Max-Age' => 86400,
                     'Access-Control-Expose-Headers' => [],
             ],

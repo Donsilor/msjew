fb780ccd738d9510297e798f29cc813dfaaeeece
gaopeng <763429951@qq.com>
2019-12-18 10:28:51 +0800

    add

--- api/config/main.php
+++ api/config/main.php
@@ -21,10 +21,12 @@ return [
     ],
     'as cors' =>[
             'class' => '\yii\filters\Cors',
-            'cors' => [
-                    'Origin' => ['*'],
-                    'Access-Control-Allow-Credentials' => true,
-            ],
+            'Origin' => ['*'],
+            'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+            'Access-Control-Request-Headers' => ['*'],
+            'Access-Control-Allow-Credentials' => null,
+            'Access-Control-Max-Age' => 86400,
+            'Access-Control-Expose-Headers' => [],
     ],
     'components' => [
         'request' => [

216503f6143d7b96d7c8ba7c88416a6390161551
zhengzhufu <fuzhuzheng@163.com>
2020-01-02 13:57:36 +0800

    订单列表

---
+++ backend/modules/order/controllers/OrderController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace backend\modules\order\controllers;
+
+use backend\controllers\BaseController;
+use common\enums\OrderStatusEnum;
+use Yii;
+use common\components\Curd;
+use common\enums\AppEnum;
+use common\enums\StatusEnum;
+use common\models\base\SearchModel;
+use common\models\order\Order;
+use yii\web\NotFoundHttpException;
+
+/**
+ * Default controller for the `order` module
+ */
+class OrderController extends BaseController
+{
+    use Curd;
+
+    /**
+     * @var Order
+     */
+    public $modelClass = Order::class;
+
+    /**
+     * Renders the index view for the module
+     * @return string
+     * @throws NotFoundHttpException
+     */
+    public function actionIndex()
+    {
+        $orderStatus = Yii::$app->request->get('order_status', -1);
+        $searchModel = new SearchModel([
+            'model' => $this->modelClass,
+            'scenario' => 'default',
+            'partialMatchAttributes' => ['sign', 'code', 'name'], // 模糊查询
+            'defaultOrder' => [
+                'id' => SORT_ASC,
+            ],
+            'pageSize' => $this->pageSize
+        ]);
+
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+        $dataProvider->query->andWhere(['>=', 'status', StatusEnum::DISABLED]);
+
+        if($orderStatus!==-1)
+            $dataProvider->query->andWhere(['=', 'order_status', $orderStatus]);
+
+        return $this->render($this->action->id, [
+            'dataProvider' => $dataProvider,
+            'searchModel' => $searchModel,
+            'orderStatus' => OrderStatusEnum::getMap(),
+        ]);
+    }
+}
---
+++ backend/modules/order/views/order/index.php
@@ -0,0 +1,90 @@
+<?php
+
+use common\enums\ConfigTypeEnum;
+use common\helpers\Url;
+use common\helpers\Html;
+use yii\grid\GridView;
+
+$this->title = '订单管理';
+$this->params['breadcrumbs'][] = ['label' => $this->title];
+?>
+
+<div class="row">
+    <div class="col-sm-12">
+        <div class="nav-tabs-custom">
+            <ul class="nav nav-tabs">
+                <li<?php if(Yii::$app->request->get('order_status', -1)==-1) echo ' class="active"' ?>><a href="<?= Url::to(['order/index']) ?>"> 全部</a></li>
+                <? foreach($orderStatus as $statusValue => $statusName) { ?>
+                    <li<?php if(Yii::$app->request->get('order_status',-1)==$statusValue) echo ' class="active"' ?>><a href="<?= Url::to(['order/index','order_status'=>$statusValue]) ?>"><?= $statusName ?></a></li>
+                <? } ?>
+            </ul>
+            <div class="tab-content">
+                <div class="active tab-pane">
+                    <?php
+                    $config = [
+                        'dataProvider' => $dataProvider,
+                        'filterModel' => $searchModel,
+                        //重新定义分页样式
+                        'tableOptions' => ['class' => 'table table-hover'],
+                        'columns' => [
+                            [
+                                'class' => 'yii\grid\SerialColumn',
+                            ],
+                            [
+                                'label' => '单号',
+                                'filter' => false, //不显示搜索框
+                                'value' => function ($model) {
+                                    return "订单号：" . $model->order_sn . '<br>' .
+                                        "支付单号：" . $model->pay_sn . '<br>' .
+                                        "物流单号：" . $model->express_no;
+                                },
+                                'format' => 'raw',
+                            ],
+                            [
+                                'label' => '商品明细',
+                                'filter' => false, //不显示搜索框
+                                'value' => function ($model) {
+                                    return "订单号：" . $model->order_sn . '<br>' .
+                                        "物流单号：" . $model->express_no;
+                                },
+                                'format' => 'raw',
+                            ],
+                            [
+                                'label' => '备注',
+                                'filter' => false, //不显示搜索框
+                                'value' => function ($model) {
+                                    return "买家：" . $model->buyer_remark . '<br>' .
+                                        "商家：" . $model->seller_remark;
+                                },
+                                'format' => 'raw',
+                            ],
+                            [
+                                'label' => '时间',
+                                'filter' => false, //不显示搜索框
+                                'value' => function ($model) {
+                                    return "创建：" . $model->created_at . '<br>' .
+                                        "支付：" . Yii::$app->formatter->asDatetime($model->payment_time) . '<br>' .
+                                        "完成：" . $model->finished_time;
+                                },
+                                'format' => 'raw',
+                            ],
+                            [
+                                'header' => "操作",
+                                'class' => 'yii\grid\ActionColumn',
+                                'template' => ' {view}',
+                                'buttons' => [
+                                    'view'=> function($url, $model, $key) {
+                                        return Html::a('预览', ['view', 'id'=>$model->id], ['class'=>'btn btn-info btn-sm']);
+                                    },
+                                ],
+                            ],
+                        ],
+                    ];
+
+                    echo GridView::widget($config);
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

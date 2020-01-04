96ed9351509e3cc5933c0119a7b7a9d39abdec01
gaopeng <763429951@qq.com>
2020-01-04 12:13:15 +0800

    add 语言包

--- common/models/order/OrderAccount.php
+++ common/models/order/OrderAccount.php
@@ -42,8 +42,9 @@ class OrderAccount extends \common\models\base\BaseModel
         return [
             [['order_id'], 'required'],
             [['order_id', 'merchant_id'], 'integer'],
-            [['order_amount', 'goods_amount', 'discount_amount', 'pay_amount', 'refund_amount', 'shipping_fee', 'tax_fee', 'safe_fee', 'other_fee'], 'number'],
+            [['order_amount', 'goods_amount', 'discount_amount', 'pay_amount', 'refund_amount', 'shipping_fee', 'tax_fee', 'safe_fee', 'other_fee','currency_rate'], 'number'],
             [['order_id'], 'unique'],
+            [['currency_code'], 'string'],
         ];
     }
 
@@ -52,18 +53,23 @@ class OrderAccount extends \common\models\base\BaseModel
      */
     public function attributeLabels()
     {
-        return [
+        
+        $currencySign = \Yii::$app->services->currency->currencySign();
+        
+        return [                
             'order_id' => '订单ID',
             'merchant_id' => '商户ID',
-            'order_amount' => '订单总金额',
-            'goods_amount' => '商品总金额',
-            'discount_amount' => '优惠金额',
-            'pay_amount' => '实际支付金额',
-            'refund_amount' => '退款金额',
-            'shipping_fee' => '运费',
-            'tax_fee' => '税费',
-            'safe_fee' => '保险费',
-            'other_fee' => '附加费',
+            'order_amount' => \Yii::t('order','订单总金额')."({$currencySign})",
+            'goods_amount' => \Yii::t('order','商品总金额')."({$currencySign})",
+            'discount_amount' => \Yii::t('order','优惠金额')."({$currencySign})",
+            'pay_amount' => \Yii::t('order','支付金额')."({$currencySign})",
+            'refund_amount' => \Yii::t('order','退款金额')."({$currencySign})",
+            'shipping_fee' => \Yii::t('order','运费')."({$currencySign})",
+            'tax_fee' => \Yii::t('order','税费')."({$currencySign})",
+            'safe_fee' => \Yii::t('order','保险费')."({$currencySign})",
+            'other_fee' => \Yii::t('order','附加费')."({$currencySign})",
+            'currency_rate'=> \Yii::t('common','汇率'),
+            'currency_code'=> \Yii::t('common','货币'),
         ];
     }
 }

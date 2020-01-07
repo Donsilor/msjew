89d171082ac29dbf37beeaa5bda1f7cbd9b89b21
gaopeng <763429951@qq.com>
2020-01-07 15:46:38 +0800

    fix order

--- backend/modules/order/views/order/index.php
+++ backend/modules/order/views/order/index.php
@@ -174,7 +174,7 @@ $this->params['breadcrumbs'][] = ['label' => $this->title];
                                     'class' => 'form-control',
                                 ]), 
                                 'value' => function ($model) {
-                                     return common\enums\PayStatusEnum::getValue($model->payment_status);
+                                    return common\enums\PayStatusEnum::getValue($model->order_status);
                                 },
                                 'format' => 'raw',
                             ],

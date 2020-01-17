44d8731c6bccae823afa7c8d633c1c6d7bb00150
zhengzhufu <fuzhuzheng@163.com>
2020-01-17 16:56:33 +0800

    test

--- common/helpers/AmountHelper.php
+++ common/helpers/AmountHelper.php
@@ -6,6 +6,14 @@ namespace common\helpers;
 
 class AmountHelper
 {
+    /**
+     * 输出金额
+     * @param float $amount 需格式化的金额
+     * @param int $rate 利率
+     * @param int $scale 保留小数位数
+     * @param string $sep sep
+     * @return string
+     */
     static public function outputAmount($amount, $rate = 1, $scale = 2, $sep = ',')
     {
         return number_format(bcmul($amount, $rate, ($scale+1)), $scale, '.', $sep);

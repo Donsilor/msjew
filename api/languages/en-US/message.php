033dca922972b4aad2141bed472ddee3decd1291
gaopeng <peng.gao@bddco.com>
2020-02-12 16:27:10 +0800

    add 语言包

--- api/languages/en-US/message.php
+++ api/languages/en-US/message.php
@@ -1,5 +1,9 @@
 <?php
 return [
-    
+    "保存成功"=>"Save successfully",
+    "保存失败"=>"Save failed",
+    "删除失败"=>"Delete failed",
+    "删除成功"=>"Delete successful",
+    "未知错误"=>"Unknown error",
     '请输入正确的邮箱地址'=>'Please enter the correct email'
 ];

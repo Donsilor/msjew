7eaa09265aed4e5e5a6f91dd3ded5a243be808d2
gaopeng <peng.gao@bddco.com>
2020-02-25 20:19:21 +0800

    fix

--- services/goods/GoodsService.php
+++ services/goods/GoodsService.php
@@ -285,7 +285,7 @@ class GoodsService extends Service
                         ->select(['g.*','IFNULL(m.sale_price,g.sale_price) as sale_price','g.goods_sn as style_sn','g.id as style_id','lang.goods_name','lang.goods_body','g.goods_num as goods_storage'])
                         ->innerJoin(DiamondLang::tableName().' lang',"g.id=lang.master_id and lang.language='{$language}'")
                         ->leftJoin(GoodsMarkup::tableName().' m','g.goods_id=m.goods_id and m.area_id='.$area_id)
-                        ->where(['goods_id'=>$goods_id])
+                        ->where(['g.goods_id'=>$goods_id])
                         ->asArray()->one();
              $goods_attr = [
                     DiamondEnum::CARAT=>$goods['carat'],

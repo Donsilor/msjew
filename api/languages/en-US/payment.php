749c2b524b2700280ee9f35a56e88a1dc5ec1fdb
zhengzhufu <fuzhuzheng@163.com>
2020-03-20 16:26:33 +0800

    语言包修改

--- api/languages/en-US/payment.php
+++ api/languages/en-US/payment.php
@@ -3,8 +3,8 @@ return [
     'PAYPAL_NOT_SUPPORT_RMB'=>'Dear BDD Co. user, as PayPal does not support RMB settlement, please switch to other currencies to complete the order payment. If you continue to use RMB settlement, please choose the China UnionPay, Alipay and WeChat to complete the order payment. Thank you for your support!',
     'PAYMENT_NOT_SUPPORT_RMB'=>'RMB is not supported temporarily, please change other currencies！',
     'PAYDOLLAR_NOT_SUPPORT_RMB_AND_USD'=>'As paydallor does not support "RMB" and "USD", please switch to other currencies to complete this order payment, thank you!',
-    'ORDER_PAID' => '此订单已经支付成功，请勿重复支付！',
-    'ORDER_STATUS_CHANGED' => '支付失败,订单状态已变更',
-    'ORDER_PAYMENT_VERIFICATION_ERROR' => '订单支付验证出错，请重试',
-    'ORDER_BEING_PAID' => '订单正在支付中...',
+    'ORDER_PAID' => 'Payment successful, do not make duplicate payment!',
+    'ORDER_STATUS_CHANGED' => 'Payment failed, order status has been changed',
+    'ORDER_PAYMENT_VERIFICATION_ERROR' => 'Unexpected error ocurred, please try it again',
+    'ORDER_BEING_PAID' => 'Payment is being processing....',
 ];

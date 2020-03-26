ceee4485994410333005c520dfccd057dce93a2b
gaopeng <763429951@qq.com>
2020-03-26 16:59:45 +0800

    语言包

---
+++ console/languages/en-US/message.php
@@ -0,0 +1,39 @@
+<?php
+return [
+    "保存成功"=>"Save successfully",
+    "保存失败"=>"Save failed",
+    "删除失败"=>"Delete failed",
+    "删除成功"=>"Delete successfully",
+    "未知错误"=>"Unknown error",
+    
+    "请输入正确的邮箱"=>"Please enter the correct email",
+    "邮箱未注册"=>"Email not registered",
+    "邮箱未绑定账号"=>"Account not bound to email",
+    "邮箱已绑定过账号"=>"Account has been bound to email",
+    "邮箱已存在"=>"Email already exists",
+    
+    "请输入正确的手机号"=>"Please input the correct mobile number",
+    "不是一个有效的手机号码"=>"Not a valid mobile number",
+    "手机号未注册"=>"Mobile number not registered",
+    "手机号未绑定账号"=>"Mobile number is not bound to account",
+    "手机号已绑定过账号"=>"Mobile number has been bound to account",
+    "手机号已存在"=>"Mobile number already exists",
+    
+    "登陆失败"=>"Login failed",
+    "登陆成功"=>"Login successfully",
+    "退出成功"=>"Exit successfully",
+    "退出失败"=>"Exit successfully",
+    "原始密码错误"=>"Original password is wrong",
+    "两次输入密码不一致"=>"The two passwords are inconsistent",    
+	
+	
+	"裸钻信息不存在或者已下架" => "Naked drilling information does not exist or has been removed",
+	"最畅销订婚戒指" => "Best selling engagement ring",
+	"商品信息不存在" => "Product information does not exist or has been removed from the market",
+	"自由搭配 爱我所爱" => "Love what I love",
+	"男士结婚戒指" => "Men's wedding ring",
+	"女士结婚戒指" => "Lady's wedding ring",
+	"对戒信息不存在" => "Warning information does not exist",
+	"精选结婚对戒" => "Selected wedding ring",
+	"精选订婚钻戒" => "Selected engagement diamond ring",
+];

a81526a8e55657ac8196b7f9c0a29305f7fad103
gaopeng <763429951@qq.com>
2020-03-26 11:47:34 +0800

    fix

--- common/components/payment/paypal/PaypalLog.php
+++ common/components/payment/paypal/PaypalLog.php
@@ -5,17 +5,17 @@ use common\helpers\FileHelper;
 
 class PaypalLog {
     
-    const LOG_PATH = \Yii::getAlias('@runtime') . "/pay-logs/paypal/".date('Ym');
+    public $logPath = \Yii::getAlias('@runtime') . "/pay-logs/paypal/".date('Ym');
     /**
      * 日志写入
      * @param unknown $message
      * @param unknown $path
      */
-    public static function writeLog($message,$file = null)
+    public static function writeLog($message,$fileName = null)
     {   
-        $filePath = $file ? self::LOG_PATH."/".$file:self::LOG_PATH."/paypal-" . date('Y-m-d') . ".log";
+        $fileName = $fileName ? $fileName :"paypal-" . date('Y-m-d') . ".log";
         $message = "[".date('Y-m-d H:i:s')."]".$message;
-        FileHelper::writeLog($filePath, $message);
+        FileHelper::writeLog(self::$logPath."/".$fileName, $message);
     }
     
 }

681fddefaff31bf34f619400f2a6c6ee3170b546
zhengzhufu <fuzhuzheng@163.com>
2020-04-08 18:27:04 +0800

    test

--- services/market/CardService.php
+++ services/market/CardService.php
@@ -24,8 +24,29 @@ class CardService extends Service
     }
 
     //生成卡密码
+    public function generatePw($prefix = '')
+    {
+        return $prefix.str_pad(mt_rand(1, 99999999999),11,'0',STR_PAD_LEFT);
+    }
+
     //生成卡号
+    public function generateSn($prefix = 'BDD')
+    {
+        return $prefix.date('Y').str_pad(mt_rand(1, 999999),6,'0',STR_PAD_LEFT);
+    }
 
     //批量生成购物卡
+
+    /**
+     * 生成购物卡
+     * @param array $card 基本数据
+     * @param int $count
+     * @param string $batch
+     */
+    public function generateCard($card, $count, $batch)
+    {
+
+    }
+
     //导出|导入数据
 }

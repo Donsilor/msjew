f75e1b882b57ae5c44eeebd4996e2e15692b2e82
gaopeng <763429951@qq.com>
2020-04-15 19:59:15 +0800

    fix

--- api/modules/web/controllers/PayController.php
+++ api/modules/web/controllers/PayController.php
@@ -67,7 +67,7 @@ class PayController extends OnAuthController
             $config = $model->getConfig();
             $trans->commit();            
             return $config;
-        }catch (Exception $e) {
+        }catch (\Exception $e) {
             $trans->rollBack();
             \Yii::$app->services->actionLog->create('用户创建支付单号',$e->getMessage());
             throw  $e;

85df149f240c8e1b5b112e29d8dcfca954227c67
祝.福 <fuzhuzheng@163.com>
2020-04-22 17:49:45 +0800

    pay

--- api/languages/zh-TW/payment.php
+++ api/languages/zh-TW/payment.php
@@ -2,7 +2,7 @@
 return [
     'PAYPAL_NOT_SUPPORT_RMB'=>'尊敬的BDD Co.用戶您好：由於paypal不支持人民幣結算，請切換其他貨幣完成本次訂單支付。如繼續使用人民幣結算，請選擇中國銀聯、支付寶、微信支付完成本次訂單支付。感謝您的支持！',
     'PAYMENT_NOT_SUPPORT_RMB'=>'人民幣暫不支持，請更換其他貨幣!',
-    'PAYDOLLAR_NOT_SUPPORT_RMB_AND_USD'=>'由於Paydallor不支持“人民幣“和“美元”進行結算，請切換其他貨幣完成本次訂單支付，謝謝！',
+    'PAYDOLLAR_NOT_SUPPORT_RMB_AND_USD'=>'由於Paydallor不支持“美元”進行結算，請切換其他貨幣完成本次訂單支付，謝謝！',
     'ORDER_PAID' => '此訂單已經支付成功，請勿重復支付！',
     'ORDER_STATUS_CHANGED' => '支付失敗,訂單狀態已變更',
     'ORDER_PAYMENT_VERIFICATION_ERROR' => '訂單支付驗證出錯，請重試',

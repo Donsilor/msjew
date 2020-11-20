a5619a69671d5974f120128e050763d3e512ead9
祝.福 <fuzhuzheng@163.com>
2020-11-20 14:36:56 +0800

    master

--- api/modules/web/controllers/goods/DiamondController.php
+++ api/modules/web/controllers/goods/DiamondController.php
@@ -323,7 +323,7 @@ class DiamondController extends OnAuthController
         $result['advert'] = array(
             'dsDesc' => '钻石——banner全屏',
             'dsImg' => '/adt/image1566005633802.png',
-            'dsName' => '鑽石——banner全屏',
+            'dsName' => '莫桑石——banner全屏',
             'dsShowType' => 1,
             'tdOpenType' => 1,
             'tdStatus' => 1,
